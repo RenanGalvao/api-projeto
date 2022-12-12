b54d24b99ad1257c0862f51a8b91c83c2ec0b762
RenanGalvao <renan.m.galvao@gmail.com>
2022-12-12 08:00:41 -0300

    fix: collaborator's field prop is obtained from user when not ADMIN (CUD methods)

--- src/collaborator/collaborator.service.ts
+++ src/collaborator/collaborator.service.ts
@@ -1,6 +1,13 @@
-import { Injectable, NotFoundException, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
-import { TEMPLATE } from 'src/constants';
+import { MESSAGE, TEMPLATE } from 'src/constants';
 import { PaginationDto } from 'src/prisma/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HardRemoveDto, RestoreDto } from 'src/utils';
@@ -10,30 +17,60 @@ import { CreateCollaboratorDto, UpdateCollaboratorDto } from './dto';
 export class CollaboratorService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async create(createCollaboratorDto: CreateCollaboratorDto) {
-    return await this.prismaService.collaborator.create({
-      data: {
+  async create(user: User, createCollaboratorDto: CreateCollaboratorDto) {
+    let dataObj = {};
+
+    if (user.role !== 'ADMIN') {
+      dataObj = {
+        ...createCollaboratorDto,
+        field: {
+          connect: {
+            id: user.fieldId,
+          },
+        },
+      };
+    } else {
+      if (!createCollaboratorDto.field) {
+        throw new BadRequestException({
+          message: TEMPLATE.VALIDATION.IS_NOT_EMPTY('field'),
+          data: {},
+        });
+      }
+
+      dataObj = {
         ...createCollaboratorDto,
         field: {
           connect: {
             id: createCollaboratorDto.field,
           },
         },
-      },
+      };
+    }
+
+    return await this.prismaService.collaborator.create({
+      data: dataObj as any,
+      include: { field: true },
     });
   }
 
   async findAll(@Query() query?: PaginationDto) {
-    return await this.prismaService.paginatedQuery('collaborator', query);
+    return await this.prismaService.paginatedQuery('collaborator', query, {
+      include: { field: true },
+    });
   }
 
   async findOne(id: string) {
     return await this.prismaService.collaborator.findUnique({
       where: { id },
+      include: { field: true },
     });
   }
 
-  async update(id: string, updateCollaboratorDto: UpdateCollaboratorDto) {
+  async update(
+    id: string,
+    user: User,
+    updateCollaboratorDto: UpdateCollaboratorDto,
+  ) {
     try {
       if (updateCollaboratorDto.field) {
         updateCollaboratorDto.field = {
@@ -43,9 +80,28 @@ export class CollaboratorService {
         delete updateCollaboratorDto.field;
       }
 
+      if (user.role !== 'ADMIN') {
+        const collaborator = await this.prismaService.collaborator.findFirst({
+          where: { id },
+        });
+
+        if (!collaborator) {
+          throw new NotFoundException({
+            message: TEMPLATE.EXCEPTION.NOT_FOUND('colaborador', 'o'),
+            data: {},
+          });
+        } else if (collaborator.fieldId !== user.fieldId) {
+          throw new ForbiddenException({
+            message: MESSAGE.EXCEPTION.FORBIDDEN,
+            data: {},
+          });
+        }
+      }
+
       return await this.prismaService.collaborator.update({
         where: { id },
         data: updateCollaboratorDto as any,
+        include: { field: true },
       });
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
@@ -60,8 +116,26 @@ export class CollaboratorService {
     }
   }
 
-  async remove(id: string) {
+  async remove(id: string, user: User) {
     try {
+      if (user.role !== 'ADMIN') {
+        const collaborator = await this.prismaService.collaborator.findFirst({
+          where: { id },
+        });
+
+        if (!collaborator) {
+          throw new NotFoundException({
+            message: TEMPLATE.EXCEPTION.NOT_FOUND('colaborador', 'o'),
+            data: {},
+          });
+        } else if (collaborator.fieldId !== user.fieldId) {
+          throw new ForbiddenException({
+            message: MESSAGE.EXCEPTION.FORBIDDEN,
+            data: {},
+          });
+        }
+      }
+
       return await this.prismaService.collaborator.delete({
         where: { id },
       });
